47c5183737b7b2ffc12729c5b08384d87a07574e
Raghu Betina <raghu@firstdraft.com>
2018-06-21 01:47:59 +0000

    Generate bookmarks resource with starter:resource

---
+++ db/migrate/20180621014759_create_bookmarks.rb
@@ -0,0 +1,13 @@
+class CreateBookmarks < ActiveRecord::Migration
+  def change
+    create_table :bookmarks do |t|
+      t.integer :user_id
+      t.integer :dish_id
+      t.integer :restaurant_id
+      t.text :notes
+
+      t.timestamps
+
+    end
+  end
+end
